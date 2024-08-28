19c662b3fbf5c5d86e72684e76e2d5a98e94ae83
Naresh Kumar M <124366029+nareshlab@users.noreply.github.com>
2024-08-28 15:52:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -60,7 +60,9 @@ def detect():
         "class_names": class_names,
         "image": encoded_image
     })
+
     
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
